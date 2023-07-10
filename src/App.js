0bf1a04421a8a8c90a644b2b38070a82d803dbed
Ahmed Abo Zaid <aabozaid0094@gmail.com>
2023-07-10 19:33:30 +0300

    Replace default content with relevant one

--- src/App.js
+++ src/App.js
@@ -1,25 +1,23 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import "./App.css";
+import Header from "./components/Header";
+import HBD from "./components/HBD";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    const [dark_schema, set_dark_schema] = React.useState(true);
+
+    const handle_schema = (event) => {
+        const dark_schema_checked = event.target.checked;
+        set_dark_schema(() => {
+            return dark_schema_checked ? true : false;
+        });
+    };
+    return (
+        <div className="App">
+            <Header dark_schema={dark_schema} handle_schema={handle_schema} />
+            <HBD dark_schema={dark_schema} />
+        </div>
+    );
 }
 
 export default App;
